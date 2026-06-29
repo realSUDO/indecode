4831f36de7622c45722876f28bc4855a8df538e5
Sumit Vishwkarma <justmultiplythinks@gmail.com>
2026-06-30 02:42:40 +0530

    [AUTH] : add redirect from root to sign-in

--- apps/auth/next.config.js
+++ apps/auth/next.config.js
@@ -13,6 +13,15 @@ const nextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/sign-in",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
